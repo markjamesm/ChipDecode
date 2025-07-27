8466f7abfade1e061e99308ab6c3cea698bf2d51
Mark-James McDougall <contact@markjames.dev>
2025-07-27 00:01:01 -0400

    Continue working on decoder.c

--- src/decoder.c
+++ src/decoder.c
@@ -6,10 +6,10 @@
 void decode_0000(const unsigned short instruction) {
     switch (instruction & 0x0FFF) {
         case 0x00E0:
-            printf("%04X detected: Clear the screen\n", instruction);
+            printf("0x%04X: Clear the screen\n", instruction);
             break;
         case 0x00EE:
-            printf("%04X detected: Return from subroutine to address pulled from stack\n", instruction);
+            printf("0x%04X: Return from subroutine to address pulled from stack\n", instruction);
             break;
         default:
             break;
@@ -24,13 +24,23 @@ void decode(const unsigned char *instruction_bytes) {
 
     const unsigned short instruction = (unsigned) instruction_bytes[0] << 8 | instruction_bytes[1];
     // printf("Decoded instruction %04X\n",instruction & 0xF000);
+    const unsigned char x = (instruction & 0x0F00) >> 8;
+    //const unsigned char y = (instruction & 0x00F0) >> 4;
+    //const unsigned char n = instruction & 0x000F;
+    const unsigned char nn = instruction & 0x00FF;
+    //const unsigned char nnn = instruction & 0x0FFF;
 
     switch (instruction & 0xF000) {
         case 0x0000:
             decode_0000(instruction);
             break;
+        case 0x6000:
+            printf("6: set v[0x%X] to 0x%X\n", x, nn);
+            break;
+        case 0x7000:
+            printf("7: Add 0x%X to v[0x%X]\n", nn, x);
         case 0xA000:
-            printf("Opcode A: Set I to %03X\n", instruction & 0x0FFF);
+            printf("A: Set I to 0x%X\n", instruction & 0x0FFF);
             break;
         default:
             printf("Unknown opcode: %04X\n", instruction);
