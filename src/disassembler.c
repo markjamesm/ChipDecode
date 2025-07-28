ba7a52770ad5d93caf911386bd870218f07d6a24
Mark-James McDougall <contact@markjames.dev>
2025-07-27 23:07:13 -0400

    Write octo assembly language to file.

--- src/disassembler.c
+++ src/disassembler.c
@@ -7,10 +7,10 @@
 void decode_0000(const unsigned short instruction, FILE *output_file) {
     switch (instruction & 0x0FFF) {
         case 0x00E0:
-            fprintf(output_file, "%04X: Clear the screen\n", instruction);
+            fprintf(output_file, "clear\n");
             break;
         case 0x00EE:
-            fprintf(output_file, "%04X: Return from subroutine to address pulled from stack\n", instruction);
+            fprintf(output_file, "return\n");
             break;
         default:
             break;
@@ -36,24 +36,24 @@ void decode(const unsigned char *instruction_bytes, FILE *output_file) {
             decode_0000(instruction, output_file);
             break;
         case 0x1000:
-            fprintf(output_file, "1: Jump to address 0x%X\n", nnn);
+            fprintf(output_file, "jump 0x%X\n", nnn);
             break;
         case 0x3000:
-            fprintf(output_file, "3: Skip next opcode if v[0x%X] == 0x%X\n", x, nn);
+            fprintf(output_file, "if v%X != 0x%X then\n", x, nn);
             break;
         case 0x6000:
-            fprintf(output_file, "6: Set v[0x%X] to 0x%X\n", x, nn);
+            fprintf(output_file, "v%X := 0x%X\n", x, nn);
             break;
         case 0x7000:
-            fprintf(output_file, "7: Add 0x%X to v[0x%X]\n", nn, x);
+            fprintf(output_file, "v%X += 0x%X\n", x, nn);
         case 0x8000:
             // Decode8000()
             break;
         case 0xA000:
-            fprintf(output_file, "A: Set I to 0x%X\n", nnn);
+            fprintf(output_file, "i := 0x%X\n", nnn);
             break;
         case 0xD000:
-            fprintf(output_file, "D: Draw (8 * 0x%X) pixel sprite at position v[0x%X], v[0x%X] with data starting at the address in I\n", n, x, y);
+            fprintf(output_file, "sprite v%X v%X 0x%X\n", x, y, n);
             break;
         default:
             printf("Unknown opcode: %04X\n", instruction);
@@ -76,6 +76,8 @@ void disassemble(const char *filename, const char *out_filename) {
         fputs("Error writing output to file\n",stderr);
         exit(EXIT_FAILURE);
     }
+
+    fprintf(output_file, ": main\n");
 
     while (fread(buffer, 1, sizeof(buffer), input_file) != 0) {
         unsigned char *instruction_bytes = malloc(sizeof(char) * 2 + 1);
