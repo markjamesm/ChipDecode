3d81e390064450a0ec890d2cdfb62417fc6fe89d
Mark-James McDougall <contact@markjames.dev>
2025-07-27 21:29:56 -0400

    - Add additional opcodes to disassembler.c

--- src/disassembler.c
+++ src/disassembler.c
@@ -24,26 +24,36 @@ void decode(const unsigned char *instruction_bytes, FILE *output_file) {
     }
 
     const unsigned short instruction = (unsigned) instruction_bytes[0] << 8 | instruction_bytes[1];
-    // printf("Decoded instruction %04X\n",instruction & 0xF000);
+
     const unsigned char x = (instruction & 0x0F00) >> 8;
-    //const unsigned char y = (instruction & 0x00F0) >> 4;
-    //const unsigned char n = instruction & 0x000F;
+    const unsigned char y = (instruction & 0x00F0) >> 4;
+    const unsigned char n = instruction & 0x000F;
     const unsigned char nn = instruction & 0x00FF;
-    //const unsigned char nnn = instruction & 0x0FFF;
+    const unsigned char nnn = instruction & 0x0FFF;
 
     switch (instruction & 0xF000) {
         case 0x0000:
             decode_0000(instruction, output_file);
             break;
+        case 0x1000:
+            fprintf(output_file, "1: Jump to address 0x%X\n", nnn);
+            break;
+        case 0x3000:
+            fprintf(output_file, "3: Skip next opcode if v[0x%X] == 0x%X\n", x, nn);
+            break;
         case 0x6000:
-            printf("6: set v[0x%X] to 0x%X\n", x, nn);
-            fprintf(output_file, "6: set v[0x%X] to 0x%X\n", x, nn);
+            fprintf(output_file, "6: Set v[0x%X] to 0x%X\n", x, nn);
             break;
         case 0x7000:
-            printf("7: Add 0x%X to v[0x%X]\n", nn, x);
-        //  fprintf(f, "7: Add 0x%X to v[0x%X]\n", nn, x);
+            fprintf(output_file, "7: Add 0x%X to v[0x%X]\n", nn, x);
+        case 0x8000:
+            // Decode8000()
+            break;
         case 0xA000:
-            printf("A: Set I to 0x%X\n", instruction & 0x0FFF);
+            fprintf(output_file, "A: Set I to 0x%X\n", nnn);
+            break;
+        case 0xD000:
+            fprintf(output_file, "D: Draw 8x[0x%X] pixel sprite at position v[0x%X], v[0x%X] with data starting at the address in I\n", n, x, y);
             break;
         default:
             printf("Unknown opcode: %04X\n", instruction);
